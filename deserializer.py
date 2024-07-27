d960e41b80493ced41bba7e4440b4c58b636eda4
Neil Martey <neil.marteybotchway@gmail.com>
2024-07-27 20:52:37 +0100

    Finish basic set of tests for deserializer

--- deserializer.py
+++ deserializer.py
@@ -10,38 +10,66 @@ class RedisException(Exception):
         if isinstance(other, RedisException):
             return self.message == other.message
         return False
-    
+
+def check_string_valid_integer(string: str):
+    """Return boolean as to whether a string represents only an integer value"""
+    stripped_int = string.lstrip("+-")
+    return stripped_int.isdigit()
+
 def deserializer(resp: str):
     def parse_simple_string(simple_string: str):
-        if simple_string[0] == '+':
+        first_byte = simple_string[0]
+        if first_byte == '+':
+            if len(simple_string) == 1:
+                # An empty simple string has been input, which is not allowed.
+                raise ValueError("RESP data cannot include empty simple strings")
             return simple_string[1:]
 
     def parse_simple_error(simple_error: str):
         if simple_error[0] == '-':
+            if len(simple_error) == 1:
+                # A simple error that does not have an error message has been input, which is not allowed.
+                raise ValueError("RESP data cannot include error objects without an error message")             
             return RedisException(simple_error[1:])
 
     def parse_integer(integer: str):
         if integer[0] == ':':
-            return int(integer[1:])
+            # Check that an actual integer has been passed through.
+            if check_string_valid_integer(integer[1:]):
+                return int(integer[1:])
+            else:
+                raise ValueError(f"RESP integer data must not be empty and must be an integer")
 
     def parse_bulk_string(resp_list: list):
-        length = int(resp_list.pop(0)[1:])
-        if length == -1:
-            return None
-        bulk_string_data = resp_list.pop(0)
-        if len(bulk_string_data) == length:
-            return bulk_string_data
+        length = resp_list.pop(0)[1:]
+        if check_string_valid_integer(length):
+            length = int(length)
+            if length == -1:
+                return None
+            bulk_string_data = resp_list.pop(0)
+            if len(bulk_string_data) == length:
+                return bulk_string_data
+            else:
+                raise ValueError("Bulk string length mismatch")
         else:
-            raise ValueError("Bulk string length mismatch")
+            raise ValueError(f"Bulk string length data should be an integer")
 
     def parse_array(resp_list: list):
-        array_length = int(resp_list.pop(0)[1:])
-        array = []
-        if array_length == - 1:
-            return None
-        for _ in range(array_length):
-            array.append(deserialize_resp(resp_list))
-        return array
+        array_length = resp_list.pop(0)[1:]
+        if check_string_valid_integer(array_length):
+            array_length = int(array_length)
+            array = []
+            if array_length == - 1:
+                return None
+            for _ in range(array_length):
+                # If there are no more items to consider within the resp list, there is an issue with the array length in the resp data
+                # Raise value error.
+                if (len(resp_list) == 0):
+                    raise ValueError("Array string length mismatch")
+                array.append(deserialize_resp(resp_list))
+            return array
+        else: 
+            raise ValueError("Array string length data should be an integer")           
 
     def tokenize(resp: str):
         return resp.split('\r\n')[:-1]
--- test_deserializer.py
+++ test_deserializer.py
@@ -2,7 +2,6 @@ import unittest
 
 from deserializer import deserializer, RedisException
 
-
 class TestDeserializer(unittest.TestCase):
     def test_valid_null(self):
         # test resp strings that are equal to null are deserialized to null
@@ -36,10 +35,59 @@ class TestDeserializer(unittest.TestCase):
         self.assertEqual(deserializer('*2\r\n$4\r\necho\r\n$11\r\nhello world\r\n'), ["echo", "hello world"])
         self.assertEqual(deserializer("*4\r\n:1\r\n:2\r\n*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n:3\r\n"), [1, 2, ['foo', 'bar'], 3])
     
+    def test_invalid_resp(self):
+        with self.assertRaises(ValueError) as cm:
+            deserializer("#2\r\n$3\r\nget\r\n$3\r\nkey\r\n")
+        error_object = cm.exception
+        self.assertEqual(str(error_object), "Unexpected token: #2")
+    
+    def test_invalid_simple_strings(self):
+        with self.assertRaises(ValueError) as cm:
+            deserializer("+\r\n")     
+        exception = cm.exception
+        self.assertEqual(str(exception), "RESP data cannot include empty simple strings")  
+    
+    def test_invalid_simple_errors(self):
+        with self.assertRaises(ValueError) as cm:
+            deserializer("-\r\n")     
+        exception = cm.exception
+        self.assertEqual(str(exception), "RESP data cannot include error objects without an error message")  
 
+    def test_invalid_integers(self):
+        # Test exception is raised for values that are not integers
+        with self.assertRaises(ValueError) as cm:
+            deserializer(":0.8\r\n")     
+        exception = cm.exception
+        self.assertEqual(str(exception), "RESP integer data must not be empty and must be an integer") 
+        # Also test for null integers
+        with self.assertRaises(ValueError) as cm:
+            deserializer(":\r\n")     
+        exception = cm.exception
+        self.assertEqual(str(exception), "RESP integer data must not be empty and must be an integer") 
 
+    def test_invalid_bulk_strings(self):
+        # Test for bulk strings where length of string is not the same as the actual length of the string
+        with self.assertRaises(ValueError) as cm:
+            deserializer("$8\r\nhello\r\n")     
+        exception = cm.exception
+        self.assertEqual(str(exception), "Bulk string length mismatch") 
+        # Test for bulk strings where the length specified isn't an integer
+        with self.assertRaises(ValueError) as cm:
+            deserializer("$5.1\r\nhello\r\n")     
+        exception = cm.exception
+        self.assertEqual(str(exception), "Bulk string length data should be an integer") 
 
-
+    def test_invalid_array(self):
+        # Test exception is raised for array length mismatch
+        with self.assertRaises(ValueError) as cm:
+            deserializer("*40\r\n$3\r\nget\r\n$3\r\nkey\r\n")     
+        exception = cm.exception
+        self.assertEqual(str(exception), "Array string length mismatch")
+        # Test exception is raised for array length that does not have an integer type
+        with self.assertRaises(ValueError) as cm:
+            deserializer("*2.5\r\n$3\r\nget\r\n$3\r\nkey\r\n")     
+        exception = cm.exception
+        self.assertEqual(str(exception), "Array string length data should be an integer")
 
 if __name__ == "__main__":
     unittest.main()
