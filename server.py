b17ea9325b8a9cdc30cf5ca8ebb9c753b226c50d
Neil Martey <neil.marteybotchway@gmail.com>
2024-07-29 18:36:39 +0100

    Implement GET and SET commands

--- server.py
+++ server.py
@@ -5,11 +5,9 @@ from deserializer import deserializer, RedisException
 HOST = "127.0.0.1"
 PORT = 6385
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"Server listening on {HOST}:{PORT}")
-    conn, addr = s.accept()
+redis_lite_dict = {}
+
+def handle_client(conn):
     with conn:
         print(f"Connected by {addr}")
         while True:
@@ -22,7 +20,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             command_word = resp_repr.pop(0)
 
             # Implement PING
-            if command_word == 'PING':
+            if command_word.upper() == 'PING':
                 if len(resp_repr) == 0:
                     # Means no parameter has been passed into ping. return pong
                     resp_response = serializer('PONG', use_bulk_str=True)
@@ -35,15 +33,48 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 conn.sendall(resp_response.encode('utf-8'))
                 print(f"Sent: {resp_response}")
             # Implement ECHO
-            elif command_word == 'ECHO':
+            elif command_word.upper() == 'ECHO':
                 if len(resp_repr) == 1:
                     # A parameter has been passed into echo. return the parameter
                     resp_response = serializer(resp_repr[0], use_bulk_str=True)
                 else:
                     # Multiple or no parameters have been passed into echo. return an error
                     resp_response = serializer(RedisException("ERR wrong number of arguments for command"))
-
                 conn.sendall(resp_response.encode('utf-8'))
                 print(f"Sent: {resp_response}")
+            # Implement SET
+            elif command_word.upper() == 'SET':
+                if len(resp_repr) == 2:
+                    # two parameters have been passed as expected. set key value pair
+                    redis_lite_dict[resp_repr[0]] = resp_repr[1]
+                    resp_response = serializer("OK", use_bulk_str=False)
+                elif len(resp_repr) < 2:
+                    resp_response = serializer(RedisException("ERR wrong number of arguments for command"))
+                else:
+                    resp_response = serializer(RedisException("syntax error"))
+                conn.sendall(resp_response.encode('utf-8'))
+                print(f"Sent: {resp_response}")
+            # Implement GET
+            elif command_word.upper() == 'GET':
+                if len(resp_repr) == 1:
+                    try:
+                        value = redis_lite_dict[resp_repr[0]]
+                    except KeyError:
+                        resp_response = serializer(None)   
+                    else:
+                        resp_response = serializer(value)                
+                else:
+                    resp_response = serializer(RedisException("ERR wrong number of arguments for command"))
+                conn.sendall(resp_response.encode('utf-8'))
+            else:
+                resp_response = serializer(RedisException(f"unknown command {command_word}"))
+                conn.sendall(resp_response.encode('utf-8'))
+            print(f"Sent: {resp_response}")
 
-                
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Server listening on {HOST}:{PORT}")
+    while True:
+        conn, addr = s.accept()
+        handle_client(conn)
